ad538dcdb05f5d59e9d3796eae77736985fd5f5e
JacobBrown <jcb99mt@byui.edu>
2025-03-13 10:53:13 -0600

    tittle and discription.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,13 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("Convolutional Neural Network Facial Recognition Application")
+
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "This is a complex facial recognition application that uses a Convolutional Neural Network to identify different faces, "
+    "using a database of images of the same person with multiple different angles, emotions, and facial expressions throughout their lifetime. "
+    "This is a great tool for security and law enforcement to identify people in a crowd or within a large database of images. "
+    "It can also be used with cameras on smart devices to increase security and privacy when accessing confidential information."
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
